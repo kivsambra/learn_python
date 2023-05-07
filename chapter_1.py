62152c66bdf97ab457152d185375cb5defd030f1
Divya <divya05abhi17@gmail.com>
2023-05-07 14:31:31 +0200

    calculate age of a person

---
+++ chapter_1.py
@@ -0,0 +1,16 @@
+""" Python has an input function ( ) which lets you ask a user for some text input
+The input() function helps to enter data at run time by the user and the output function
+print() is used to display the result of the program on the screen after execution
+
+Husband , wife  and date of birth recives in input in output display the result
+"""
+name=input('please enter your name')
+year_of_birth=input('please enter your year of birth')
+print(f'person name: {name}')
+current_year=2023
+y=int(year_of_birth)
+print(current_year-y)
+
+
+
+
